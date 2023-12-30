595d00c4cb26e951e5ea1cf7ff12f7fa8d4368fb
Sophie Hanna Langbein <79644349+sophhan@users.noreply.github.com>
2023-12-30 17:37:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -50,9 +50,8 @@ var confetti = new ConfettiGenerator(confettiSettings);
 
 confetti.render();
 
-document.addEventListener("DOMContentLoaded", function () {
-  let p = document.createElement("p");
-  p.innerHTML = 'Congrats you made the right choice 🎉 <br> Your prize is spending a day with me at <a href="https://action-arena.info">Action Arena Norderstedt</a> and <a href="https://www.misspepper.de">Miss PeppeR American Diner</a>';
+ let p = document.createElement("p");
+ p.innerHTML = 'Congrats you made the right choice 🎉 <br> Your prize is spending a day with me at <a href="https://action-arena.info">Action Arena Norderstedt</a> and <a href="https://www.misspepper.de">Miss PeppeR American Diner</a>';
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
